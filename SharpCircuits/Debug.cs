ae87e2d2648fa8a9122e6a4040e3a6b5c04381e4
Tomas Mariancik <tomas.mariancik@gmail.com>
2021-09-04 07:28:47 +0200

    Removed Program class from the class library and made Debug Log functions conditional to the debug mode only

---
+++ SharpCircuits/Debug.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace SharpCircuit
+{
+	public static class Debug
+	{
+		[Conditional("DEBUG")]
+		public static void Log(params object[] objs)
+		{
+			StringBuilder sb = new StringBuilder();
+			foreach (object o in objs)
+				sb.Append(o.ToString()).Append(" ");
+			Console.WriteLine(sb.ToString());
+		}
+
+		[Conditional("DEBUG")]
+		public static void LogF(string format, params object[] objs)
+		{
+			Console.WriteLine(string.Format(format, objs));
+		}
+
+	}
+}
