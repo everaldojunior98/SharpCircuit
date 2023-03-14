bf47b503269a90a010b48714ddbb0c5ff63364da
Everaldo Júnior <everaldo.junior98@live.com>
2023-03-14 11:13:47 -0300

    DC Motor
    Adding dc motor based on https://github.com/sharpie7/circuitjs1/blob/master/src/com/lushprojects/circuitjs1/client/DCMotorElm.java

---
+++ SharpCircuits/src/InductorBase.cs
@@ -0,0 +1,61 @@
+﻿namespace SharpCircuit
+{
+    public class InductorBase
+    {
+        private double inductance;
+        private bool isTrapezoidal;
+
+        private int[] nodes;
+        private double compResistance;
+        private double current;
+        private double curSourceValue;
+
+        public InductorBase(double induc, double c, bool trapezoidal)
+        {
+            nodes = new int[2];
+            inductance = induc;
+            current = c;
+            isTrapezoidal = trapezoidal;
+        }
+
+        public void reset()
+        {
+            current = 0;
+        }
+
+        public void stamp(Circuit sim, int n0, int n1)
+        {
+            nodes[0] = n0;
+            nodes[1] = n1;
+
+            if (isTrapezoidal)
+                compResistance = 2 * inductance / sim.timeStep;
+            else
+                compResistance = inductance / sim.timeStep;
+
+            sim.stampResistor(nodes[0], nodes[1], compResistance);
+            sim.stampRightSide(nodes[0]);
+            sim.stampRightSide(nodes[1]);
+        }
+
+        public void beginStep(Circuit sim, double voltdiff)
+        {
+            if (isTrapezoidal)
+                curSourceValue = voltdiff / compResistance + current;
+            else
+                curSourceValue = current;
+        }
+
+        public double calculateCurrent(double voltdiff)
+        {
+            if (compResistance > 0)
+                current = voltdiff / compResistance + curSourceValue;
+            return current;
+        }
+
+        public void doStep(Circuit sim)
+        {
+            sim.stampCurrentSource(nodes[0], nodes[1], curSourceValue);
+        }
+    }
+}
---
+++ SharpCircuits/src/elements/DCMotor.cs
@@ -0,0 +1,133 @@
+﻿using static SharpCircuit.Circuit;
+
+namespace SharpCircuit.elements
+{
+    // based on https://github.com/sharpie7/circuitjs1/blob/master/src/com/lushprojects/circuitjs1/client/DCMotorElm.java
+
+    public class DCMotor : CircuitElement
+    {
+        public Lead leadIn => lead0;
+
+        public Lead leadOut => lead1;
+
+        public double angle { get; private set; }
+        public double speed { get; private set; }
+
+        private InductorBase ind;
+        private InductorBase indInertia;
+
+        // Electrical parameters
+        private double resistance;
+        private double inductance;
+
+        // Electro-mechanical parameters
+        private double K;
+        private double Kb;
+        private double J;
+        private double b;
+
+        private double coilCurrent;
+        private double inertiaCurrent;
+        private readonly int[] voltSources;
+
+        public DCMotor()
+        {
+            inductance = 0.5;
+            resistance = 1;
+            angle = pi / 2;
+            speed = 0;
+            K = 0.15;
+            b = 0.05;
+            J = 0.02;
+            Kb = 0.15;
+
+            voltSources = new int[2];
+
+            ind = new InductorBase(inductance, 0, false);
+            indInertia = new InductorBase(J, 0, false);
+
+            allocLeads();
+        }
+
+        public double getAngle()
+        {
+            return angle;
+        }
+
+        public override int getInternalLeadCount()
+        {
+            return 4;
+        }
+
+        public override int getVoltageSourceCount()
+        {
+            return 2;
+        }
+
+        public override void setVoltageSource(int leadX, int voltSourceNdx)
+        {
+            voltSources[leadX] = voltSourceNdx;
+        }
+
+        public override void reset()
+        {
+            ind.reset();
+            indInertia.reset();
+            coilCurrent = 0;
+            inertiaCurrent = 0;
+        }
+
+        public override void stamp(Circuit sim)
+        {
+            // stamp a bunch of internal parts to help us simulate the motor.  It would be better to simulate this mini-circuit in code to reduce
+            // the size of the matrix.
+
+            //nodes[0] nodes [1] are the external nodes
+            //Electrical part:
+            // inductor from motor nodes[0] to internal nodes[2]
+            ind.stamp(sim, lead_node[0], lead_node[2]);
+            // resistor from internal nodes[2] to internal nodes[3] // motor post 2
+            sim.stampResistor(lead_node[2], lead_node[3], resistance);
+            // Back emf voltage source from internal nodes[3] to external nodes [1]
+            sim.stampVoltageSource(lead_node[3], lead_node[1], voltSources[0]); // 
+
+            //Mechanical part:
+            // inertia inductor from internal nodes[4] to internal nodes[5]
+            indInertia.stamp(sim, lead_node[4], lead_node[5]);
+            // resistor from  internal nodes[5] to  ground 
+            sim.stampResistor(lead_node[5], 0, b);
+            // Voltage Source from  internal nodes[4] to ground
+            sim.stampVoltageSource(lead_node[4], 0, voltSources[1]);
+        }
+
+        public override void beginStep(Circuit sim)
+        {
+            ind.beginStep(sim, lead_volt[0] - lead_volt[2]);
+            indInertia.beginStep(sim, lead_volt[4] - lead_volt[5]);
+
+            angle += speed * sim.timeStep;
+        }
+
+        public override void step(Circuit sim)
+        {
+            sim.updateVoltageSource(lead_node[4], 0, voltSources[1], coilCurrent * K);
+            sim.updateVoltageSource(lead_node[3], lead_node[1], voltSources[0], inertiaCurrent * Kb);
+            ind.doStep(sim);
+            indInertia.doStep(sim);
+        }
+
+        public override void calculateCurrent()
+        {
+            coilCurrent = ind.calculateCurrent(lead_volt[0] - lead_volt[2]);
+            inertiaCurrent = indInertia.calculateCurrent(lead_volt[4] - lead_volt[5]);
+
+            speed = inertiaCurrent;
+        }
+
+        public override void setCurrent(int voltSourceNdx, double c)
+        {
+            if (voltSourceNdx == voltSources[0])
+                current = c;
+        }
+    }
+}
